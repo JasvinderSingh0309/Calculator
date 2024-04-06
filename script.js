a994a312b9628e07c7d85af6b4ef37b7d50f3298
Jasvinder Singh <jasvinder4251@gmail.com>
2024-04-06 13:03:15 +0530

    added css and used localStorage

--- script.js
+++ script.js
@@ -1,54 +1,94 @@
 let btns = document.querySelectorAll("button");
 let display = document.querySelector(".display");
+let calc = localStorage.getItem("c") || "";
 
-let calc = "";
+function showError() {
+  display.textContent = "E";
+  localStorage.setItem("c",`E`);
+  calc = "";
+}
 
 function contentToCalculate(value) {
-  if(value === "Clear") {
+  if(value === "clear") {
     display.textContent = 0;
+    localStorage.setItem("c","");
     calc = "";
   }else if(value === "="){
-    if(!calc) {
+    if(localStorage.getItem("c") === "E") {
+      return;
+    }
+    if(!calc){
       display.textContent = 0;
-    }else {
+    }else{
+      let firstLetter = calc[0];
       let lastLetter = calc[calc.length-1];
       let secLastLetter = calc[calc.length-2];
 
-      if(lastLetter === "+" || lastLetter === "-" || lastLetter === "/" || lastLetter === "*" || lastLetter === "%") {
-        display.textContent = "E";
-        calc = "";
-      }else if(lastLetter === "0" && secLastLetter === "/") {
-        display.textContent = "E";
-        calc = "";
-      }else { 
+      if(lastLetter === "+" || lastLetter === "-" || lastLetter === "/" || lastLetter === "*" || lastLetter === "%" || lastLetter === ".") {
+        showError();
+      } else if(lastLetter === "0" && secLastLetter === "/") {
+        showError();
+      } else if(firstLetter === "*" || firstLetter === "/") {
+        showError();
+      } else { 
         let flag = true;
         for(let i=1;i<calc.length;i++) {
-          if((calc[i] === '+' || calc[i] === '-' || calc[i] === '*' || calc[i] === '/' || calc[i] === '%') && (calc[i-1] === '+' || calc[i-1] === '-' || calc[i-1] === '*' || calc[i-1] === '/' || calc[i-1] === '%')) {
+          if((calc[i] === '+' || calc[i] === '-' || calc[i] === '*' || calc[i] === '/' || calc[i] === '%' || calc[i] === '.') && (calc[i-1] === '+' || calc[i-1] === '-' || calc[i-1] === '*' || calc[i-1] === '/' || calc[i-1] === '%' || calc[i-1] === '.')) {
             flag = false;
           }
         }
+
         if(flag) {
-          let result = eval(calc);
+          let result = eval(localStorage.getItem("c"));
+        
           if(!Number.isInteger(result)) {
             let check = (Math.floor(result * 10000))%10;
             if(check !== 0) {
               result = (Math.floor(result * 1000))/1000;
             }
           }
+
+          if(`${result}` === "Infinity" || `${result}` === "NaN" || `${result}` === "null") {
+            showError();
+            return;
+          }
+          
           display.textContent = result;
-          calc = "";
+          if(result === 0) {
+            localStorage.setItem("c",'');
+            calc = "";
+          }else{
+            localStorage.setItem("c",`${result}`);
+            calc = `${result}`;
+          }
         }else {
-          display.textContent = 'E';
-          calc = "";
+          showError();
         }
       }
     }
   }else if(value === "x") {
+    calc = localStorage.getItem("c");
+    if(calc === "E") {
+      calc = "";
+    }
     calc += "*";
     display.textContent = calc;
+    localStorage.setItem("c",`${calc}`);
   }else{
-    calc += value;
-    display.textContent = calc;
+    if(value === '0' && (!localStorage.getItem("c") || localStorage.getItem("c") === 'E')) {
+      if(calc === "" && display.textContent !== "0") {
+        localStorage.setItem("c","E");
+        return;
+      }
+      localStorage.setItem("c",``);
+    }else{
+      if(calc === 'E') {
+        calc = "";
+      }
+      calc += value;
+      display.textContent = calc;
+      localStorage.setItem("c",`${calc}`);
+    }
   }
 }
 
@@ -57,3 +97,10 @@ btns.forEach( btn => {
     contentToCalculate(btn.textContent);
   })
 })
+
+window.addEventListener("load",() => {
+  if(localStorage.getItem("c") && localStorage.getItem("c") !== "0")
+    display.textContent = localStorage.getItem("c");
+})
+
+
